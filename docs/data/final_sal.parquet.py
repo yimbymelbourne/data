1a741c35da52968f9f88dfe55e82e7c161c24e48
Max Bo <mjbo@canva.com>
2024-02-23 11:25:28 +1100

    Lets gooo

--- docs/data/final_sal.parquet.py
+++ docs/data/final_sal.parquet.py
@@ -4,13 +4,14 @@ import sys
 import pyarrow.feather as feather
 import pyarrow.parquet as pq
 
+# We need to convert Tom's feather file to parquet and write it to stdout:
+# https://observablehq.com/framework/loaders
 response = requests.get(
     "https://github.com/tpisel/walkability/raw/master/data/final/final_sal.feather", 
     allow_redirects=True)
 
-# Open the file in write mode 
+# write in binary mode
 with open('docs/data/final_sal.feather', 'wb') as file:
-    # Write the contents of the response to the file
     file.write(response.content)
 
 feather_table = feather.read_table('docs/data/final_sal.feather')
@@ -18,9 +19,7 @@ pq.write_table(feather_table, 'docs/data/temp_final_sal.parquet')
 
 # read in binary mode
 with open('docs/data/temp_final_sal.parquet', 'rb') as file:
-    # read the file
     data = file.read()
-    # write bytes to stdout
     sys.stdout.buffer.write(data)
 
 os.remove('docs/data/final_sal.feather')
