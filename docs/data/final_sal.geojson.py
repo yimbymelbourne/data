8a8ee820387a0eda596cb97627c9038c63fbe49a
Max Bo <mjbo@canva.com>
2024-02-23 13:14:53 +1100

    Hmmmmmm

---
+++ docs/data/final_sal.geojson.py
@@ -0,0 +1,27 @@
+import os
+import requests
+import sys
+import geopandas as gpd
+
+# We need to convert Tom's feather file to parquet and write it to stdout:
+# https://observablehq.com/framework/loaders
+response = requests.get(
+    "https://github.com/tpisel/walkability/raw/master/data/final/final_sal.feather", 
+    allow_redirects=True)
+
+# write in binary mode
+with open('docs/data/final_sal.feather', 'wb') as file:
+    file.write(response.content)
+
+# Use geopandas to read the feather file
+gdf = gpd.read_feather('docs/data/final_sal.feather')
+
+gdf.to_file("docs/data/temp_final_sal.geojson", driver='GeoJSON')
+
+# read in binary mode
+with open('docs/data/temp_final_sal.geojson', 'rb') as file:
+    data = file.read()
+    sys.stdout.buffer.write(data)
+
+os.remove('docs/data/final_sal.feather')
+os.remove('docs/data/temp_final_sal.geojson')
--- docs/walkability.md
+++ docs/walkability.md
@@ -6,6 +6,7 @@ title: Walkability
 
 ```js
 const final_sal_parquet = FileAttachment("data/final_sal.parquet").parquet()
+const final_sat_geojson = FileAttachment("data/final_sal.geojson").json();
 ```
 
 Let's try and reconstruct the parks and pubs visualization from
@@ -29,4 +30,29 @@ Plot.plot({
     }),
   ],
 })
+```
+
+
+```js
+import * as L from "npm:leaflet";
+
+const div = display(document.createElement("div"));
+div.style = "height: 400px;";
+
+const map = L.map(div)
+  .setView([-37.8136, 144.9631], 13);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png")
+  .addTo(map);
+
+const style = {
+    "color": "#ff7800",
+    "weight": 5,
+    "opacity": 0.65
+};
+
+// TODO: Figure out why nothings appearing.
+L.geoJSON(final_sat_geojson).addTo(map, {
+    style
+});
 ```
