fd152dd23b42c07288d5edcf9b7fa569903a123c
Max Bo <mjbo@canva.com>
2024-02-28 01:00:50 +1100

    More loaders

---
+++ docs/data/final_sa1.parquet.py
@@ -0,0 +1,6 @@
+from loaders import print_feather_file_to_stdout
+
+name = "final_sa1"
+url = f"https://github.com/tpisel/walkability/raw/master/data/final/{name}.feather" 
+
+print_feather_file_to_stdout(name=name, url=url, type="parquet")
