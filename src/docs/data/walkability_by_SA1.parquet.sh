d85ab599b06f5120a47dc3eef170139fbeb0dc30
Max Bo <mjbo@canva.com>
2024-08-04 14:08:42 +1000

    kill docs

--- src/docs/data/walkability_by_SA1.parquet.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl 'https://tompisel.com/data/walkability_by_SA1.parquet'
--- src/docs/data/walkability_by_SAL.geojson.py
+++
@@ -1,6 +0,0 @@
-from loaders import print_parquet_file_to_stdout
-
-name = "walkability_by_SAL"
-url = 'https://tompisel.com/data/walkability_by_SAL.parquet'
-
-print_parquet_file_to_stdout(name=name, url=url, type="geojson")
